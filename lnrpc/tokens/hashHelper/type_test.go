437f32fbf045f862f6ae2fb8ee08250a0315460e
IntegralTeam <repo@sfxdx.com>
2021-09-08 16:33:36 +0300

    feat: describe hash types

--- lnrpc/tokens/hashHelper/type_test.go
+++ lnrpc/tokens/hashHelper/type_test.go
@@ -2,8 +2,51 @@ package hashhelper
 
 import "testing"
 
-func TestValidate(t *testing.T) {
-	var a address = "some"
+func TestAddress(t *testing.T) {
+	a, err := NewAddress("pkt1qjucsc48vg6njy9djp4utwyzatgk6qdzeu9unmg")
+	if err != nil {
+		t.Fatalf("on create address %v", err)
+	}
 
-	a.validate()
+	isAddress := a.validate()
+	if !isAddress {
+		t.Fatalf("hash %v created as a address", a)
+	}
+
+}
+
+func TestBlock(t *testing.T) {
+	b, err := NewBlock("blocksome")
+	if err != nil {
+		t.Fatalf("on create block %v", err)
+	}
+
+	isBlock := b.validate()
+	if !isBlock {
+		t.Fatalf("hash %v created as a block", b)
+	}
+}
+
+func TestTransaction(t *testing.T) {
+	tx, err := NewTransaction("txsome")
+	if err != nil {
+		t.Fatalf("on create transaction %v", err)
+	}
+
+	isTransaction := tx.validate()
+	if !isTransaction {
+		t.Fatalf("hash %v created as a transaction", tx)
+	}
+}
+
+func TestLock(t *testing.T) {
+	l, err := NewLock("locksome")
+	if err != nil {
+		t.Fatalf("on create lock %v", err)
+	}
+
+	isLock := l.validate()
+	if !isLock {
+		t.Fatalf("hash %v created as a lock", l)
+	}
 }
--- lnrpc/tokens/hashHelper/types.go
+++ lnrpc/tokens/hashHelper/types.go
@@ -1,22 +1,63 @@
 package hashhelper
 
+import (
+	"fmt"
+	"strings"
+)
+
 type address string
 type transaction string
 type block string
 type lock string
 
+func NewAddress(hash string) (*address, error) {
+	a := address(hash)
+	if !a.validate() {
+		return nil, fmt.Errorf("hash %v not a address", hash)
+	}
+
+	return &a, nil
+}
+
+func NewBlock(hash string) (*block, error) {
+	b := block(hash)
+	if !b.validate() {
+		return nil, fmt.Errorf("hash %v not a block", hash)
+	}
+
+	return &b, nil
+}
+
+func NewTransaction(hash string) (*transaction, error) {
+	t := transaction(hash)
+	if !t.validate() {
+		return nil, fmt.Errorf("hash %v not a transaction", hash)
+	}
+
+	return &t, nil
+}
+
+func NewLock(hash string) (*lock, error) {
+	l := lock(hash)
+	if !l.validate() {
+		return nil, fmt.Errorf("hash %v not a lock", hash)
+	}
+
+	return &l, nil
+}
+
 func (a *address) validate() bool {
-	return true
+	return strings.HasPrefix(string(*a), "pkt1")
 }
 
 func (t *transaction) validate() bool {
-	return true
+	return strings.HasPrefix(string(*t), "tx")
 }
 
 func (b *block) validate() bool {
-	return true
+	return strings.HasPrefix(string(*b), "block")
 }
 
 func (l *lock) validate() bool {
-	return true
+	return strings.HasPrefix(string(*l), "lock")
 }
